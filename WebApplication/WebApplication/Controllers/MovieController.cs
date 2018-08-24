12657a8381322aaa635814f19ed092e5e9f90ba0
Kamalpreet <kamalpreet.singh@kelltontech.com>
2018-08-24 18:51:38 +0530

    first commit

---
+++ WebApplication/WebApplication/Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Kellton.Models.Details;
+
+namespace Kellton.WebApplication.Controllers
+{
+    public class MovieController: Controller
+    {
+        List<MovieModel> movieList = new List<MovieModel>();
+
+        public IActionResult Index()
+        {
+            MovieData movieData = new MovieData();
+            movieList=movieData.getMovieList();
+            return View(movieList);
+        }
+    }
+}
